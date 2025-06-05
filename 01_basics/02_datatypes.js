8a707497f832582850c2ea87836f069fb2507166
Ahmedraza330 <ahmed.argdynamics@gmail.com>
2025-06-05 23:12:32 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict" // treat all JS code as newer versions
+
+// alert (3 + 3) // we are using node js not browser
+
+console.log (3 + 3) // code readability should be high
+
+
+console.log("Ahmed");
+
+
+
+let name = "Ahmed" // string
+let age = 19 // number
+let isloggedin = true //boolean
+let state = null // null
+let address; // undefined
+
+// Primitive Datatypes
+/*
+number => 2 to power 53
+bigint => for bigger number
+string => ""
+boolean => true/false
+null => standalone value
+undefined => the value is'nt defined
+symbol => unique 
+*/
+
+// object
+
+
+console.log(typeof age) // to find the type of variables
+
+console.log(typeof null); // null is basically object in type 
+
+console.log(typeof undefined); // undefined is also undefined in type 
+
+
+
