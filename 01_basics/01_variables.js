7eef2cd6145fc70746721e6e30e85d2472d5e83f
Ahmedraza330 <ahmed.argdynamics@gmail.com>
2025-06-05 22:41:47 +0000

    topic_variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = "01"
+let accountName = "Ahmed Raza"
+var accountAge = "20" //  Prefer should not to use var
+accountState = "Karachi" // not readable
+let accountAddress; // Undefined
+
+
+console.table ([accountId, accountName, accountAge, accountState, accountAddress])
